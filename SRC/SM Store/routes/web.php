815cdc480c104cd96e8b3f68d8655a96121938e2
kcicpp <23010546@st.phenikaa-uni.edu.vn>
2025-09-06 18:01:03 +0700

    tái cấu trúc

--- SRC/SM Store/routes/web.php
+++ SRC/SM Store/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home.index');
+    return view('page.home.index');
 });
 Route::get('/community', function () {
     return view('page.community.index');
